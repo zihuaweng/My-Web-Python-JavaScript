ee25a3ee88d42f7ad454c72fe1a5ff12a4a4be3f
zihuaweng <wengzihua123@126.com>
2020-02-28 21:42:32 -0800

    temp

--- pages/models.py
+++ pages/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 
 class Page(models.Model):
-    text = models.TextField()
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
+    body = models.TextField()
 
     def __str__(self):
-        return self.text[:50]
+        return self.title
